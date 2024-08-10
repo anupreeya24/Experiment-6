d781d6901f8563e3595eeda23a4eb939aed23787
anupreeya24 <anupreeyawo@gmail.com>
2024-08-10 18:40:55 +0530

    Create star_shape1.cpp

---
+++ star_shape1.cpp
@@ -0,0 +1,29 @@
+//Anupreeya Bhattacharjee
+//23070123022
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"Enter your choice: ";
+    cin>>n;
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+/*
+Enter your choice: 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
